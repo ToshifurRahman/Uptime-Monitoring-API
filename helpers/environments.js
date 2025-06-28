e4526c4aa744106be1fe0949f8945257c9d1d0a9
Md. Toshifur Rahman <tfrtasin11@gmail.com>
2025-06-28 21:27:12 +0600

    Added Checks

--- helpers/environments.js
+++ helpers/environments.js
@@ -14,12 +14,14 @@ environments.staging = {
     port: 3000,
     envName: 'staging',
     secretKey: 'waeruqwoiecnvnkrljtjj',
+    maxChecks: 5,
 };
 
 environments.production = {
     port: 5000,
     envName: 'production',
     secretKey: 'vbcvbxcvuiovbuoirwerw',
+    maxChecks: 5,
 };
 
 // determine which environment was passed
