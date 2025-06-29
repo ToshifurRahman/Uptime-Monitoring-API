70de2c731001da1518115e7e95510d0a2d104409
Md. Toshifur Rahman <tfrtasin11@gmail.com>
2025-06-29 13:47:45 +0600

    added worker process

--- index.js
+++ index.js
@@ -1,34 +1,25 @@
 /*
- * Title: Uptime Monitoring Application
- * Description: A RESTful API to monitor up or down time of user defined links
+ * Title: Project Initial file
+ * Description: Initial file to start the node server and workers
  * Author: Md. Toshifur Rahman
- * Date: 26/06/2025
+ * Date: 29/06/2025
  *
  */
 // dependencies
-const http = require('http');
-const { handleReqRes } = require('./helpers/handleReqRes');
-const environment = require('./helpers/environments');
-const { sendTwilioSms } = require('./helpers/notifications');
+const server = require('./lib/server');
+const workers = require('./lib/worker');
 
 // app object - module scaffolding
 const app = {};
 
-// Twilio Send Request
-// sendTwilioSms('01700540970', 'Hello World', (err) => {
-//     console.log('This is the error', err);
-// });
-
-// create server
-app.createServer = () => {
-    const server = http.createServer(app.handleReqRes);
-    server.listen(environment.port, () => {
-        console.log(`listening to port ${environment.port}`);
-    });
+app.init = () => {
+    // start the server
+    server.init();
+    // start the workers
+    workers.init();
 };
 
-// handle Request Response
-app.handleReqRes = handleReqRes;
+app.init();
 
-// start the server
-app.createServer();
+// export the app
+module.exports = app;
